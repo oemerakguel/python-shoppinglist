444149a1849165c80e322fdf0fab98e7954613d6
Ömer Akgül <oemer.akguel@gmx.de>
2024-10-02 15:21:17 +0200

    modified add main function

--- shoppinglist.py
+++ shoppinglist.py
@@ -16,3 +16,24 @@ def show_shoppinglist():
             print(f"{item}")
 
 show_shoppinglist()
+
+
+def main():
+    while True:
+        print("-----Einkaufsliste-----")
+        print("1. Artikel zur Einkaufslsite hinzufügen")
+        print("2. Einkaufsliste anzeigen")
+        print("3. Programm beenden")
+
+        choice = input("Was möchten Sie tun ? ")
+
+        if choice == "1":
+            item = input("Bitte gib ein Artikel ein, der zur Einkaufsliste hinzugeügt werden soll:")
+            add_item(item)
+        elif choice == "2":
+            shoppinglist()
+        elif choice == "3":
+            print("Programm wird beendet! Auf Wiedersehen")
+            exit()
+        elif not choice == "1" or choice == "2" or choice == "3":
+            print("Ungültige Auswahl. Bitte wähle 1, 2 oder 3")
