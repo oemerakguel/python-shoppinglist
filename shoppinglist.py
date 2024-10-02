7816d336ed1226c4430ca7abeafcf23496de8c15
Ömer Akgül <oemer.akguel@gmx.de>
2024-10-02 13:52:01 +0200

    inserted add_item function

--- shoppinglist.py
+++ shoppinglist.py
@@ -1,1 +1,10 @@
 shoppinglist = []
+
+def add_item(item):
+    shoppinglist.append(item)
+    print(f"'{item}' wurde zur Einkauflsite hinzugefügt.")
+
+item = input("Bitte gib ein Artikel ein, der zur Einkaufsliste hinzugeügt werden soll:")
+add_item(item)
+
+print(shoppinglist)
